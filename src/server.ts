4810dcec820d7e6e98c49f6349928c3d09c5f498
Jefferson Candido <jefferson.ufu@gmail.com>
2021-05-31 22:09:15 -0300

    Testing VSCode Debug

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  return res.send({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333');
+});
